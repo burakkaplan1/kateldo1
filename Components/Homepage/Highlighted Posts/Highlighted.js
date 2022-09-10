7b0f49e1b63a9155ad865400832a3b3786da04a3
Burak Kaplan <burakkaplanwork@gmail.com>
2022-09-10 23:45:06 +0200

    [KAT-2] Translation and Router is added

--- Components/Homepage/Highlighted Posts/Highlighted.js
+++ Components/Homepage/Highlighted Posts/Highlighted.js
@@ -1,11 +1,14 @@
 import { onSnapshot, query, collection } from "firebase/firestore";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useEffect, useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import { db } from "../../../firebase";
 import HomepageCard from "./HomepageCard";
 
-function Highlighted() {
+function Highlighted(props) {
+  const { t } = useTranslation();
   const [tab, setTab] = useState("All");
   const [houses, setHouses] = useState([]);
   const [vehicles, setVehicles] = useState([]);
@@ -33,9 +36,13 @@ function Highlighted() {
     <div className="px-2">
       <Tabs>
         <TabList>
-          <Tab onClick={() => setTab("All")}>All</Tab>
-          <Tab onClick={() => setTab("RealEstate")}>Real Estate</Tab>
-          <Tab onClick={() => setTab("Vehicle")}>Vehicle</Tab>
+          <Tab onClick={() => setTab("All")}>{t("All", { ns: "Common" })}</Tab>
+          <Tab onClick={() => setTab("RealEstate")}>
+            {t("RealEstate", { ns: "Common" })}
+          </Tab>
+          <Tab onClick={() => setTab("Vehicle")}>
+            {t("Vehicles", { ns: "Common" })}
+          </Tab>
         </TabList>
 
         <TabPanel>
@@ -43,7 +50,8 @@ function Highlighted() {
             {houses.slice(0, currentShow).map((house) => (
               <HomepageCard
                 key={house.id}
-                cardType="RealEstate"
+                id={house.id}
+                cardType="realestate"
                 image={
                   house.data().ImageURL
                     ? house.data().ImageURL
@@ -60,55 +68,61 @@ function Highlighted() {
                 numberOfFloor={house.data().NumberOfFloor}
                 RoomNumber={house.data().NumberOfRoom}
                 BedroomNumber={house.data().NumberOfBedroom}
+                BathroomNumber={house.data().NumberOfBathroom}
+                EnergyLevel={house.data().EnergyLevel}
                 LivingroomNumber={house.data().NumberOfLivingRoom}
               />
             ))}
-            {vehicles.slice(0, currentShow).map((vehicle) => (
-              <HomepageCard
-                key={vehicle.id}
-                cardType="Vehicle"
-                image={
-                  vehicle.data().ImageURL
-                    ? vehicle.data().ImageURL
-                    : vehicle.data().ImageArray[0]
-                }
-                Price={vehicle.data().Price}
-                VehicleType={vehicle.data().BodyType}
-                firstItem={vehicle.data().Brand}
-                secondItem={vehicle.data().Model}
-                thirdItem={
-                  Intl.NumberFormat("nl-NL", {
-                    minimumFractionDigits: 0,
-                    maximumFractionDigits: 0,
-                  }).format(vehicle.data().Kilometers) + " Km"
-                }
-                fourthItem={vehicle.data().ModelYear}
-                GearType={vehicle.data().GearType}
-                FuelType={vehicle.data().FuelType}
-                EngineSize={vehicle.data().EngineDisplacement}
-                Color={vehicle.data().ExteriorColor}
-                MaximumSpeed={vehicle.data().MaximumSpeed}
-                EVRange={vehicle.data().ElectricRange}
-                Torque={
-                  vehicle.EngineIncluded?.System &&
-                  vehicle.data().Torque?.System?.Value !== undefined
-                    ? vehicle.data().Torque?.System?.Value
-                    : Object.values(omit("System", vehicle.data().Torque)[0])
-                        .map((engine) => engine.Value)
-                        .filter((engine) => engine !== undefined)
-                        .reduce((accumulator, curr) => accumulator + curr)
-                }
-                Horsepower={
-                  vehicle.data().EngineIncluded?.System &&
-                  vehicle.data().Power?.System?.HorsePower !== undefined
-                    ? vehicle.data().Power?.System?.HorsePower
-                    : Object.values(omit("System", vehicle.data().Power)[0])
-                        .map((engine) => engine.HorsePower)
-                        .filter((engine) => engine)
-                        .reduce((accumulator, curr) => accumulator + curr)
-                }
-              />
-            ))}
+            <>
+              {vehicles.slice(0, currentShow).map((vehicle) => (
+                <HomepageCard
+                  key={vehicle.id}
+                  id={vehicle.id}
+                  cardType="vehicle"
+                  image={
+                    vehicle.data().ImageURL
+                      ? vehicle.data().ImageURL
+                      : vehicle.data().ImageArray[0]
+                  }
+                  Price={vehicle.data().Price}
+                  VehicleType={vehicle.data().BodyType}
+                  firstItem={vehicle.data().Brand}
+                  secondItem={vehicle.data().Model}
+                  thirdItem={
+                    Intl.NumberFormat("nl-NL", {
+                      minimumFractionDigits: 0,
+                      maximumFractionDigits: 0,
+                    }).format(vehicle.data().Kilometers) + " Km"
+                  }
+                  fourthItem={vehicle.data().ModelYear}
+                  GearType={vehicle.data().GearType}
+                  FuelType={vehicle.data().FuelType}
+                  EngineSize={vehicle.data().EngineDisplacement}
+                  Color={vehicle.data().ExteriorColor}
+                  InteriorColor={vehicle.data().InteriorColor}
+                  MaximumSpeed={vehicle.data().MaximumSpeed}
+                  EVRange={vehicle.data().ElectricRange}
+                  Torque={
+                    vehicle.EngineIncluded?.System &&
+                    vehicle.data().Torque?.System?.Value !== undefined
+                      ? vehicle.data().Torque?.System?.Value
+                      : Object.values(omit("System", vehicle.data().Torque)[0])
+                          .map((engine) => engine.Value)
+                          .filter((engine) => engine !== undefined)
+                          .reduce((accumulator, curr) => accumulator + curr)
+                  }
+                  Horsepower={
+                    vehicle.data().EngineIncluded?.System &&
+                    vehicle.data().Power?.System?.HorsePower !== undefined
+                      ? vehicle.data().Power?.System?.HorsePower
+                      : Object.values(omit("System", vehicle.data().Power)[0])
+                          .map((engine) => engine.HorsePower)
+                          .filter((engine) => engine)
+                          .reduce((accumulator, curr) => accumulator + curr)
+                  }
+                />
+              ))}
+            </>
           </div>
         </TabPanel>
         <TabPanel>
@@ -116,7 +130,8 @@ function Highlighted() {
             {houses.slice(0, currentShow).map((house) => (
               <HomepageCard
                 key={house.id}
-                cardType="RealEstate"
+                id={house.id}
+                cardType="realestate"
                 image={
                   house.data().ImageURL
                     ? house.data().ImageURL
@@ -133,6 +148,8 @@ function Highlighted() {
                 numberOfFloor={house.data().NumberOfFloor}
                 RoomNumber={house.data().NumberOfRoom}
                 BedroomNumber={house.data().NumberOfBedroom}
+                BathroomNumber={house.data().NumberOfBathroom}
+                EnergyLevel={house.data().EnergyLevel}
                 LivingroomNumber={house.data().NumberOfLivingRoom}
               />
             ))}
@@ -143,7 +160,8 @@ function Highlighted() {
             {vehicles.slice(0, currentShow).map((vehicle) => (
               <HomepageCard
                 key={vehicle.id}
-                cardType="Vehicle"
+                id={vehicle.id}
+                cardType="vehicle"
                 image={
                   vehicle.data().ImageURL
                     ? vehicle.data().ImageURL
@@ -164,18 +182,9 @@ function Highlighted() {
                 FuelType={vehicle.data().FuelType}
                 EngineSize={vehicle.data().EngineDisplacement}
                 Color={vehicle.data().ExteriorColor}
-                ModelYear={vehicle.data().ModelYear}
+                InteriorColor={vehicle.data().InteriorColor}
                 MaximumSpeed={vehicle.data().MaximumSpeed}
                 EVRange={vehicle.data().ElectricRange}
-                Horsepower={
-                  vehicle.data().EngineIncluded?.System &&
-                  vehicle.data().Power?.System?.HorsePower !== undefined
-                    ? vehicle.data().Power?.System?.HorsePower
-                    : Object.values(omit("System", vehicle.data().Power)[0])
-                        .map((engine) => engine.HorsePower)
-                        .filter((engine) => engine)
-                        .reduce((accumulator, curr) => accumulator + curr)
-                }
                 Torque={
                   vehicle.EngineIncluded?.System &&
                   vehicle.data().Torque?.System?.Value !== undefined
@@ -183,6 +192,15 @@ function Highlighted() {
                     : Object.values(omit("System", vehicle.data().Torque)[0])
                         .map((engine) => engine.Value)
                         .filter((engine) => engine !== undefined)
+                        .reduce((accumulator, curr) => accumulator + curr)
+                }
+                Horsepower={
+                  vehicle.data().EngineIncluded?.System &&
+                  vehicle.data().Power?.System?.HorsePower !== undefined
+                    ? vehicle.data().Power?.System?.HorsePower
+                    : Object.values(omit("System", vehicle.data().Power)[0])
+                        .map((engine) => engine.HorsePower)
+                        .filter((engine) => engine)
                         .reduce((accumulator, curr) => accumulator + curr)
                 }
               />
@@ -197,7 +215,7 @@ function Highlighted() {
             className="px-3 py-2 border-2 dark:text-white text-blue-800 border-blue-300 hover:bg-blue-500 hover:text-white"
             onClick={() => setCurrentShow(currentShow - 3)}
           >
-            Show less
+            {t("ShowLess", { ns: "Common" })}
           </button>
         )}
         {currentShow <= 12 && (
@@ -205,7 +223,7 @@ function Highlighted() {
             className="px-3 py-2 border-2 dark:text-white text-blue-800 border-blue-300 hover:bg-blue-500 hover:text-white"
             onClick={() => setCurrentShow(currentShow + 3)}
           >
-            Show more
+            {t("ShowMore", { ns: "Common" })}
           </button>
         )}
       </div>
